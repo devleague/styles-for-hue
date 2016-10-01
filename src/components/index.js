bc5ef8077f8532fc9195c343502e961fb5d485e1
Nikki K <808nikz@gmail.com>
2016-09-30 22:40:18 -1000

    exported the header and footer

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as PComp } from './PComp';
 export { default as ImgComp } from './ImgComp';
 export { default as ListComp } from './ListComp';
 export { default as NavLink } from './NavLink';
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
