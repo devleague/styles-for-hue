0f6786e4439de7081ac8bda006c45b00a6f1aeba
Nikki K <808nikz@gmail.com>
2016-10-01 17:07:07 -1000

    working

--- src/components/ImgComp.js
+++ src/components/ImgComp.js
@@ -3,17 +3,26 @@ import React, { Component } from 'react';
 class ImgComp extends Component {
   render () {
     return (
-      <img
-        className="imgComp"
-        src="http://buzzsharer.com/wp-content/uploads/2015/07/cute-corgi-face.jpg"
-        alt="I like rice!"
+      <div
         style={
           {
-            display: "inline-block",
-            width: "30%"
+            margin: "30px"
           }
         }
-      />
+      >
+        <img
+          className="imgComp"
+          // src="http://buzzsharer.com/wp-content/uploads/2015/07/cute-corgi-face.jpg"
+          // alt="I like rice!"
+          style={
+            {
+              display: "inline-block",
+              width: "960px",
+              height: "360px"
+            }
+          }
+        />
+      </div>
     )
   }
 }
