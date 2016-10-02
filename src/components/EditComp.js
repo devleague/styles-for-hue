6f39d14e00a9d1593d827c0d5e6534eb28da25ae
Nikki K <808nikz@gmail.com>
2016-10-01 15:25:24 -1000

    Adding the Edit bar for the template

---
+++ src/components/EditComp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class EditComp extends Component {
+  render () {
+    return (
+      <div
+        className="editColumn"
+      >
+        <h1> Edit </h1>
+        <div>
+          <h3>Pick Your Font:</h3>
+        </div>
+        <div>
+          <h3>Pick Your Color:</h3>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EditComp;
