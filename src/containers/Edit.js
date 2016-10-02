4912ad23bdd5eb287ff4bd9de7420854163c35b3
Bryce <bsaito88@gmail.com>
2016-10-01 18:06:23 -1000

    Moved change methods outside of constructor.

--- src/containers/Edit.js
+++ src/containers/Edit.js
@@ -4,22 +4,14 @@ import React, { Component } from 'react';
 class Edit extends Component {
   constructor (props) {
     super(props);
-    this.colorValue = () => {
-      var color = document.getElementById('colorMenu').value;
-      document.getElementById("Color").innerHTML = "You selected: " + color;
-      document.getElementById("Color").style.backgroundColor = color;
-      return color;
-    }
-    this.fontValue = () => {
-      var fontMenu = document.getElementById('fontMenu').value;
-      document.getElementById("Font").innerHTML = "You selected: " + font;
-      document.getElementById("Font").style.fontFamily = font;
-      return font;
-    }
   }
-  ChangeFont() {
+  changeFont() {
       var font = document.getElementById("fontMenu").value;
       return font;
+  }
+  changeColor() {
+      var color = document.getElementById("colorMenu").value;
+      return color;
   }
   render () {
     return (
@@ -29,19 +21,17 @@ class Edit extends Component {
         <h1> Edit </h1>
         <div>
           <h3>Pick Your Font:</h3>
-            <select id="fontMenu" onChange={this.ChangeFont}>
+            <select id="fontMenu" onChange={this.changeFont}>
               <option value="times">Times New Roman</option>
               <option value="arial">Arial</option>
             </select>
-            <div id="Font"></div>
         </div>
         <div>
           <h3>Pick Your Color:</h3>
-            <select id="colorMenu" onChange={() => this.props.changeColor(this.colorValue())}>
+            <select id="colorMenu" onChange={() => this.props.changeColor(this.changeColor())}>
               <option value="blue">Blue</option>
               <option value="purple">Purple</option>
             </select>
-            <div id="Color"></div>
         </div>
       </div>
     )
