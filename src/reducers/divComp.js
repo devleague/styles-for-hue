e2fec5d8544a09399438b028cc61840796cccee9
Bryce <bsaito88@gmail.com>
2016-10-01 14:04:01 -1000

    Created actions and reducers folder with some reducer code.

---
+++ src/reducers/divComp.js
@@ -0,0 +1,16 @@
+const initialState = {
+  display: 'inline-block',
+  backgroundColor: 'red'
+};
+
+const reducer = (state = initialState, action) => {
+  let newState = { ...state };
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      newState.backgroundColor = action.data.backgroundColor;
+    default:
+      return newState;
+  }
+}
+
+export default reducer;
