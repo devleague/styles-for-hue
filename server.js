4bb3e2e1e558840bd496af18069749cbc555a558
Nikki K <808nikz@gmail.com>
2016-10-01 16:01:32 -1000

    testing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', function (req, res) {
 })
 
 app.use((req, res) => {
-  res.status(404).send('Fix Dis Stuff!');
+  res.status(404).render('Fix Dis Stuff!');
 })
 
 const server = app.listen(app.get('port'), () => {
