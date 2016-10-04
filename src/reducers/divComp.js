f55d9cd45e77ae9237b92f7962c27b5b04c69b72
Bryce <bsaito88@gmail.com>
2016-10-03 17:25:35 -1000

    Fixed reducers to change div format.

--- src/reducers/divComp.js
+++ src/reducers/divComp.js
@@ -3,19 +3,22 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  let newState = { ...state };
+  let newDivs = { ...state}.divs;
   switch (action.type) {
     case "SET_DIVS":
-      newState.divs = action.data;
-      return newState;
+      return { ...state, divs: action.data};
     case "CHANGE_COLOR":
-      newState.backgroundColor = action.data.backgroundColor;
-      return newState;
+      newDivs = newDivs.map((div, index) => {
+        return { style: { ...div.style, backgroundColor: action.data.backgroundColor}};
+      });
+      return { ...state, divs: newDivs};
     case "CHANGE_FONT":
-      newState.fontFamily = action.data.fontFamily;
-      return newState;
+      newDivs = newDivs.map((div, index) => {
+        return { style: { ...div.style, fontFamily: action.data.fontFamily}};
+      });
+      return { ...state, divs: newDivs};
     default:
-      return newState;
+      return { ...state};
   }
 }
 
