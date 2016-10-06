4746eb1b17f83f24c6f344484e12ccd3344f1bf4
Christie R <creindle@hawaii.edu>
2016-10-05 22:08:32 -1000

    When selecting elements, styles show beneath

--- src/containers/Edit.js
+++ src/containers/Edit.js
@@ -76,15 +76,6 @@ class Edit extends Component {
             Get Element
           </button>
         </div>
-        <div>
-          <button
-            className="showCSS"
-            type="button"
-            onClick={this.showElementStyles}
-          >
-            Show Element Style
-          </button>
-        </div>
       </div>
     )
   }
