97fc8f02c7fbfe2869c3348cb9b1f820c7ce35be
Christie R <creindle@hawaii.edu>
2016-10-08 19:53:32 -1000

    Inline styles are exported as SCSS and no filenames gives back date as name

--- src/containers/Edit.js
+++ src/containers/Edit.js
@@ -29,7 +29,7 @@ class Edit extends Component {
     })
   }
 
-  btnSave(elements) {
+  exportAsSCSSFile(elements) {
     var text = "";
     var li = "liTags";
     var liStyles = "";
@@ -37,96 +37,26 @@ class Edit extends Component {
       var elementType = key.toString();
       if (elementType === "ulTags") {
         for (var i = 0; i < elements[key][0].subType.length; i++) {
-          liStyles += "liTags: " + JSON.stringify(elements[key][0].subType[i].style) + "\n";
+          liStyles += "liTags " + JSON.stringify(elements[key][0].subType[i].style) + "\n";
 
         }
       }
       for (var i = 0; i < elements[key].length; i++){
         var elementStyle = (JSON.stringify(elements[key][i].style));
-        text += elementType + ": " + elementStyle + "\n";
+        text += elementType + " " + elementStyle + "\n";
       }
     }
     text += liStyles;
+    text = text.replace(/['"]+/g, '');
+    text = text.replace(/,/g, ';\n  ');
+
     var filename = document.getElementById("input-fileName").value;
+    if (filename === "") {
+      filename = new Date().toTimeString();
+    }
     var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-    fileSaver.saveAs(blob, filename+".txt");
+    fileSaver.saveAs(blob, filename+".scss");
   };
-
-  exportAsCSSFile() {
-    var incomingString = 'background-color: red; font-family: arial; display: inline-block;';
-
-    var testArray = [
-      {
-        elementId: 1,
-        type: 'div',
-        style: {
-          backgroundColor: 'blue',
-          fontFamily: 'sans-serif',
-          display: 'inline-block'
-        }
-      },
-      {
-        elementId: 2,
-        type: 'p',
-        style: {
-          backgroundColor: 'green',
-          fontFamily: 'arial',
-        }
-      },
-      {
-        elementId: 3,
-        type: 'img',
-        src: '/images/city_night.jpg',
-        style: {
-          width: "100%",
-          backgroundColor: "#3C6E71"
-        }
-      },
-      {
-        elementId: 4,
-        type: 'ul',
-        subType: [
-          {
-            elementId: 6,
-            type: 'li',
-            style: {
-              backgroundColor: 'pink'
-            }
-          },
-          {
-            elementId: 7,
-            type: 'li',
-            style: {
-              backgroundColor: 'orange'
-            }
-          }
-        ],
-        style: {
-          color: 'red'
-        }
-      },
-      {
-        elementId: 5,
-        type: 'div',
-        style: {
-          backgroundColor: 'red',
-          fontFamily: 'arial',
-          display: 'inline-block'
-        }
-      }
-    ];
-
-    console.log(testArray);
-
-
-    // var lineBreaks = incomingString.replace(/; /g, ';\n  ');
-    // var initialBracket = '{ \n  ';
-    // var endBracket = '\n}';
-    // var initialSCSS = initialBracket.concat(lineBreaks);
-    // var finishedSCSS = initialSCSS.concat(endBracket);
-
-    // console.log(finishedSCSS);
-  }
 
   render() {
     return (
@@ -156,19 +86,16 @@ class Edit extends Component {
             Saved!
           </div>
         </div>
-        <form role="form">
-          <div class="form-group">
-            <label for="input-fileName">File name</label>
+        <form>
+          <div>
+            <label>File name</label>
             <input type="text"
-              class="form-control"
               id="input-fileName"
               placeholder="Enter file name"></input>
           </div>
           <button
-            id="btn-save"
             type="submit"
-            class="btn btn-primary"
-            onClick={()=> this.btnSave(this.props.elements)}>Save to file</button>
+            onClick={()=> this.exportAsSCSSFile(this.props.elements)}>Save to file</button>
         </form>
       </div>
     )
