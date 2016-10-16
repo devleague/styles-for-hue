22513f5e2c70b1d9ee4653c011ad09348dbcf387
Nikki K <808nikz@gmail.com>
2016-10-15 18:45:48 -1000

    test

--- src/containers/Edit.js
+++ src/containers/Edit.js
@@ -199,7 +199,7 @@ class Edit extends Component {
         </div>
         <div
           className="div-menu"
-        >
+        > 
           <span>
             <button
               id="button-show"
