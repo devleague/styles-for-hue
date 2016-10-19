268f0bd13e715a0193cd19c263ca46887fd2611c
Lisa <misslisazhou@gmail.com>
2016-10-19 12:34:58 -1000

    remove unused divs

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -20,25 +20,21 @@ class Home extends Component {
           </div>
         </div>
         <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12">
-                <ul className="list-inline">
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li className="footer-menu-divider">&sdot;</li>
-                  <li>
-                    <a href="#">Template</a>
-                  </li>
-                  <li className="footer-menu-divider">&sdot;</li>
-                  <li>
-                    <a href="#">About</a>
-                  </li>
-                </ul>
-                <p>Copyright &copy; 2016 Styles For Hue. All Rights Reserved</p>
-              </div>
-            </div>
+          <div className="footer-content">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li className="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="#">Template</a>
+              </li>
+              <li className="footer-menu-divider">&sdot;</li>
+              <li>
+                <a href="#">About</a>
+              </li>
+            </ul>
+            <p>Copyright &copy; 2016 Styles For Hue. All Rights Reserved</p>
           </div>
         </footer>
       </div>
