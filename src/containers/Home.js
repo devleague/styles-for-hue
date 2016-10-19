9928ce0ef847a2f1da9b0a8d63017470be3d6fc0
Lisa <misslisazhou@gmail.com>
2016-10-18 21:02:00 -1000

    remove unused div

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,40 +5,33 @@ class Home extends Component {
     return (
       <div>
         <div
-          className="home-container"
+          className="home-background"
         >
           <div
-            className="home-background"
+            className="home-title"
           >
-            <div
-              className="home-title"
-            >
-              <h5>Welcome To</h5>
-              <h1>Styles For Hue</h1>
-            </div>
+            <h5>Welcome To</h5>
+            <h1>Styles For Hue</h1>
           </div>
         </div>
-        <footer
-        >
+        <footer>
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
                 <ul className="list-inline">
-                    <li>
-                      <a href="#">Home</a>
-                    </li>
-                    <li className="footer-menu-divider">&sdot;</li>
-                    <li>
-                      <a href="#">Template</a>
-                    </li>
-                    <li className="footer-menu-divider">&sdot;</li>
-                    <li>
-                      <a href="#">About</a>
-                    </li>
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li className="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="#">Template</a>
+                  </li>
+                  <li className="footer-menu-divider">&sdot;</li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
                 </ul>
-                <p>
-                  Copyright &copy; 2016, Styles For Hue. All Rights Reserved
-                </p>
+                <p>Copyright &copy; 2016, Styles For Hue. All Rights Reserved</p>
               </div>
             </div>
           </div>
