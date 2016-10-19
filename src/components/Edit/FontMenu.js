557f67bad224868816c09d90123ac372531e2930
Bryce <bsaito88@gmail.com>
2016-10-18 21:50:58 -1000

    Able to modify fonts on either h tag or p tag.
    - Added condition to read for the tag on the element.
    - Works for fontFamily, color, and fontSize.

--- src/components/Edit/FontMenu.js
+++ src/components/Edit/FontMenu.js
@@ -2,33 +2,36 @@ import React, { Component } from 'react';
 
 class FontMenu extends Component {
   changeFontFamily(fontFamily) {
+    var elementTag = document.getElementById("elementMenu").value
     var font = {
       fontFamily: fontFamily
     };
-    return this.switchThemeFonts(font);
+    return this.switchThemeFonts(elementTag, font);
   }
 
   changeFontColor(fontColor) {
+    var elementTag = document.getElementById("elementMenu").value
     var font = {
       color: fontColor
     }
-    return this.switchThemeFonts(font);
+    return this.switchThemeFonts(elementTag, font);
   }
   changeFontSize(fontSize) {
+    var elementTag = document.getElementById("elementMenu").value
     if (fontSize !== 'em' && fontSize !== 'px' && fontSize !== '') {
       var font = {
         fontSize: `${fontSize + document.getElementById("fontSizeUnits").value}`
       }
-      return this.switchThemeFonts(font);
+      return this.switchThemeFonts(elementTag, font);
     }
   }
   // changeFontSizeUnit() {
   //   return font;
   // }
 
-  switchThemeFonts(fontObj) {
+  switchThemeFonts(elementTag, fontObj) {
     // this.props.changeSelectedFont(newFont);
-    console.log(fontObj);
+    console.log(elementTag);
     let newElems = this.props.elements;
     newElems = newElems.map((elem) => {
       if (elem.children) {
@@ -39,16 +42,31 @@ class FontMenu extends Component {
                 secondChild.children = secondChild.children.map((thirdChild) => {
                   if(thirdChild.children) {
                     thirdChild.children = thirdChild.children.map((fourthChild) => {
-                      switch (Object.keys(fontObj)[0]) {
-                        case 'fontFamily':
-                          return { ...fourthChild, style: { ...fourthChild.style, fontFamily: fontObj.fontFamily}};
-                        case 'color':
-                          return { ...fourthChild, style: { ...fourthChild.style, color: fontObj.color}};
-                        case 'fontSize':
-                          return { ...fourthChild, style: { ...fourthChild.style, fontSize: fontObj.fontSize}};
-                        default:
-                          return { ...fourthChild};
+                      if (elementTag === 'all') {
+                        switch (Object.keys(fontObj)[0]) {
+                          case 'fontFamily':
+                            return { ...fourthChild, style: { ...fourthChild.style, fontFamily: fontObj.fontFamily}};
+                          case 'color':
+                            return { ...fourthChild, style: { ...fourthChild.style, color: fontObj.color}};
+                          case 'fontSize':
+                            return { ...fourthChild, style: { ...fourthChild.style, fontSize: fontObj.fontSize}};
+                          default:
+                            return { ...fourthChild};
+                        }
                       }
+                      if (elementTag === fourthChild.tag[0]) {
+                        switch (Object.keys(fontObj)[0]) {
+                          case 'fontFamily':
+                            return { ...fourthChild, style: { ...fourthChild.style, fontFamily: fontObj.fontFamily}};
+                          case 'color':
+                            return { ...fourthChild, style: { ...fourthChild.style, color: fontObj.color}};
+                          case 'fontSize':
+                            return { ...fourthChild, style: { ...fourthChild.style, fontSize: fontObj.fontSize}};
+                          default:
+                            return { ...fourthChild};
+                        }
+                      }
+                      return { ...fourthChild};
                     })
                   }
                   return { ...thirdChild};
@@ -68,6 +86,13 @@ class FontMenu extends Component {
   render() {
     return(
       <div>
+        <h3>Pick Your Element:</h3>
+          <select id="elementMenu" defaultValue="0">
+            <option value="0" disabled="disabled">SELECT ELEMENT</option>
+            <option value='all'>all tags</option>
+            <option value='p'>p tags</option>
+            <option value='h'>h tags</option>
+          </select>
         <h3>Pick Your Font:</h3>
           <select id="fontMenu" defaultValue="0" onChange={(event) => this.changeFontFamily(event.target.value)}>
             <option value="0" disabled="disabled">SELECT FONT</option>
