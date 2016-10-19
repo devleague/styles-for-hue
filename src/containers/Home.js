61c9deb3f7a2e2d9af1c97bc1ed8bed3f86cf246
Lisa <misslisazhou@gmail.com>
2016-10-18 20:44:00 -1000

    add more divs and content for footer

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,26 +3,46 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div
-        className="home-container"
-      >
+      <div>
         <div
-          className="home-background"
+          className="home-container"
         >
           <div
-            className="home-title"
+            className="home-background"
           >
-            <h5>Welcome To</h5>
-            <h1>Styles For Hue</h1>
+            <div
+              className="home-title"
+            >
+              <h5>Welcome To</h5>
+              <h1>Styles For Hue</h1>
+            </div>
           </div>
         </div>
-        <div
-          className="home-footer"
+        <footer
         >
-          <p>
-            &copy; 2016 Styles For Hue
-          </p>
-        </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <ul className="list-inline">
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li>
+                      <a href="#">Template</a>
+                    </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li>
+                      <a href="#">About</a>
+                    </li>
+                </ul>
+                <p>
+                  Copyright &copy; 2016, Styles For Hue. All Rights Reserved
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     )
   }
