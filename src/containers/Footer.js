db049095c629acaf4a71611bcc0d7b633d6db2a5
Lisa <misslisazhou@gmail.com>
2016-10-21 01:05:21 -1000

    create reusable Footer component

---
+++ src/containers/Footer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+          <div className="footer-content">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li className="footer-menu-divider">&sdot;</li>
+              <li>
+                <Link to="/template">Template</Link>
+              </li>
+              <li className="footer-menu-divider">&sdot;</li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+            <p>Copyright &copy; 2016 Styles For Hue. All Rights Reserved</p>
+          </div>
+        </footer>
+    )
+  }
+}
+
+export default Footer;
