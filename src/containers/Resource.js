7cdcacf79101d38102bb577b7c3b74f952ab0e56
Nikki K <808nikz@gmail.com>
2016-10-22 09:54:35 -1000

    Creating a container for the Resource Route

---
+++ src/containers/Resource.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Resource extends Component {
+  render() {
+    return (
+      <div
+        className="content-container"
+      >
+        <div
+          className="resource-image"
+        >
+          <h1>Resources</h1>
+        </div>
+        <div
+          className="row"
+        >
+          <div
+            className="flex-nowrap-container"
+          >
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Resource;
