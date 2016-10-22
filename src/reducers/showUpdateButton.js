94462d39dba9976950e6f11b5982ba2194b62287
Nikki K <808nikz@gmail.com>
2016-10-21 18:01:58 -1000

    Removing the case for the hide update button

--- src/reducers/showUpdateButton.js
+++ src/reducers/showUpdateButton.js
@@ -6,8 +6,6 @@ const reducer = ( state = initialState, action) => {
   switch (action.type) {
     case "SHOW_UPDATEBUTTON":
       return {...state, updateTemp: action.show};
-    case "HIDE_UPDATEBUTTON":
-      return {...state, updateTemp: action.show}
     default:
       return state;
   }
