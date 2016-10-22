8f3bcc72ca7eb166bd29240a7d6a0185350b6675
Bryce <bsaito88@gmail.com>
2016-10-22 10:52:56 -1000

    Added clickHandler to Footer.

--- src/components/Template/Footer.js
+++ src/components/Template/Footer.js
@@ -4,9 +4,10 @@ class Footer extends Component {
   render () {
     return (
       <div
+        id={this.props.elementId}
         className={this.props.className}
         style={this.props.style}
-        onClick={() => this.props.selectElement(this.props.elementId, this.props.style)}
+        onClick={(event) => this.props.clickHandler(event, this.props.selectElement, this.props.style)}
       >
         <h6>
           Copyright © 2016 YOUR COMPANY. All Rights Reserved
