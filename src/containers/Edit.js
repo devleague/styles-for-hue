96681fe404c074e5935fe1bdf20b72eb2bb58a73
Nikki K <808nikz@gmail.com>
2016-10-22 14:16:16 -1000

    Temporarily removed the view css button

--- src/containers/Edit.js
+++ src/containers/Edit.js
@@ -245,18 +245,6 @@ class Edit extends Component {
         className="editColumn"
       >
         <h1> Edit </h1>
-          <button
-            className="view-css"
-            onClick={ ()=> {
-              if (this.props.sideBar.showCss === false) {
-                this.props.showCss(true);
-              } else {
-                this.props.showCss(false);
-              }
-            }
-          }>
-            &lt;CSS&gt;
-          </button>
           <div className="dropdown">
           <button className="dropbtn"
             onClick={this.changeUser}>Dropdown</button>
