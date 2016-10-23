9e587ca159ee9ff4893799c111c39ece26189a1d
Sheena <sheena.galutira@gmail.com>
2016-10-22 20:41:19 -1000

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,6 @@ app.get('/api/template/:id', (req, res) => {
 
 app.put('/api/template/:id', (req, res) => {
   let id = req.params.id;
-  console.log(req.body.template);
   UserTemplate.findOneAndUpdate(id, {doc: req.body.template})
   .then((results) => {
     return res.json(results);
