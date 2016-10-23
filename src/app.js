b020bf2652b65ffb1bc770558530e2d8cdb29bd8
Nikki K <808nikz@gmail.com>
2016-10-22 21:17:28 -1000

    Creating the route for the 404

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ ReactDOM.render(
         >
         </Route>
         <Route
-          path="*" component={ NotFound }
+          status={ 404 } path="*" component={ NotFound }
         />
       </Route>
     </Router>
