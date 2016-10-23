9f553aabd9f2450b68ae81ec165b383db2baa549
Nikki K <808nikz@gmail.com>
2016-10-22 19:31:48 -1000

    Putting spaces around the NotFound for the component part of the Route

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ ReactDOM.render(
         >
         </Route>
         <Route
-          path="*" component={NotFound}
+          path="*" component={ NotFound }
         />
       </Route>
     </Router>
