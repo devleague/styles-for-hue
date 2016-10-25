08e8606b0844418c67913f4aa28bfb10c14b9e1b
Nikki K <808nikz@gmail.com>
2016-10-24 19:57:15 -1000

    Renaming the file to tutorial

---
+++ src/containers/Tutorial.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Tutorial extends Component {
+  render() {
+    return (
+      <div
+        className="content-container"
+      >
+        <div
+          className="header-image tutorial-image"
+        >
+          <h1>Tutorial</h1>
+        </div>
+        <div
+          className="row"
+        >
+          <div
+            className="flex-wrap-container"
+          >
+            <div
+              className="tutorial=container"
+            >
+              <h1></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Tutorial;
