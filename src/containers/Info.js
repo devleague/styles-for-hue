3539f43fda334ec83cb49c8d1df4c0bf94f2be70
Nikki K <808nikz@gmail.com>
2016-10-24 19:37:55 -1000

    Importing react and react-router

---
+++ src/containers/Info.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Info extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default Info;
