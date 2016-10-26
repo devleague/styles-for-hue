cf8ed7a78bb961c71ec3f1bb758b946abcafe147
Lisa <misslisazhou@gmail.com>
2016-10-25 18:58:24 -1000

    changed div classnames and h2 to h3

--- src/containers/About.js
+++ src/containers/About.js
@@ -13,12 +13,14 @@ class About extends Component {
           <h1>About Us</h1>
         </div>
         <div className="row">
-          <div className="flex-nowrap-container">
-            <div className="flex-50 left">
+          <div className="flex-wrap-container">
+            <div className="flex-100">
               <h2>Who We Are</h2>
+            </div>
+            <div className="flex-50">
               <p>Oat cake dragée cheesecake chocolate cake. Jelly dessert dessert chocolate liquorice caramels sweet ice cream liquorice. Gummi bears tart topping gummies jelly bear claw. Lemon drops tootsie roll topping. Dragée gingerbread jelly beans cake dragée cake caramels. Icing sesame snaps bonbon sweet roll.</p>
             </div>
-            <div className="flex-50 right">
+            <div className="flex-50">
               <p>Marzipan bear claw sweet donut. Jujubes croissant candy wafer cheesecake caramels carrot cake tart. Apple pie chocolate apple pie gummi bears. Candy biscuit cheesecake gummi bears powder. Tootsie roll sugar plum chocolate cake jelly beans chupa chups bear claw apple pie.</p>
             </div>
           </div>
@@ -27,27 +29,27 @@ class About extends Component {
           <div className="flex-wrap-container">
             <div className="flex-photos pos-1">
               <div className="profile"/>
-              <h2>Nikki Kobayashi</h2>
+              <h3>Nikki Kobayashi</h3>
               <p>Stuff I Did</p>
             </div>
             <div className="flex-photos pos-2">
               <div className="profile"/>
-              <h2>Lisa Zhou</h2>
+              <h3>Lisa Zhou</h3>
               <p>Stuff I Did</p>
             </div>
             <div className="flex-photos pos-3">
               <div className="profile"/>
-              <h2>Bryce Saito</h2>
+              <h3>Bryce Saito</h3>
               <p>Stuff I Did</p>
             </div>
             <div className="flex-photos pos-4">
               <div className="profile"/>
-              <h2>Christie Reindle</h2>
+              <h3>Christie Reindle</h3>
               <p>Stuff I Did</p>
             </div>
             <div className="flex-photos pos-5">
               <div className="profile"/>
-              <h2>Sheena Galutira</h2>
+              <h3>Sheena Galutira</h3>
               <p>Stuff I Did</p>
             </div>
           </div>
