b200027c6ea6b7bd71a592143ac8447133f00826
Lisa <misslisazhou@gmail.com>
2016-11-07 01:55:56 -1000

    remove br tags, change h3 to h2 tags, removed unnecessary content

--- src/containers/Tutorial.js
+++ src/containers/Tutorial.js
@@ -25,41 +25,32 @@ class Tutorial extends Component {
               </div>
             </div>
             <div className="flex-100">
-              <br/>
-              <h1>Welcome to Styles for Hue</h1>
-              <br />
-            </div>
-            <div className="flex-100">
-              <h3>Getting Started</h3>
+              <h2 className="content-subtitle">Getting Started</h2>
               <p>On the <NavLink to="/">Home</NavLink> page click on the button Start Styling or <NavLink to="/template">Template</NavLink> on the navigation bar.</p>
               <p>Use the <i>Edit Sidebar</i> and click on the icons next to the Font, Div, Images and Saved Templates to show the menu to change styles.</p>
-              <br />
             </div>
             <div className="flex-100">
-              <h3>Font</h3>
-              <p>Pick your element: Change your fonts based on the type of element (&lt;p&gt;, &lt;h1&gt;, &lt;h2&gt;, &lt;h3&gt;, selected, or all).</p>
+              <h2 className="content-subtitle">Font</h2>
+              <p>Pick your element: Change your fonts based on the type of element (&lt;p&gt;, &lt;h1&gt;, &lt;h2&gt;, &lt;h2&gt;, selected, or all).</p>
               <p>Pick your font: Choose a font.</p>
               <p>Font color: Change colors using the color wheel.</p>
               <p>Font size: Type in desired font size number.</p>
-              <br />
             </div>
             <div className="flex-100">
-              <h3>Div</h3>
+              <h2 className="content-subtitle">Div</h2>
               <p>Pick your palette: Use preset palettes to change div colors.</p>
               <p>Pick your color: Select a div and change the color of the individual div.</p>
-              <br />
             </div>
             <div className="flex-100">
-              <h3>Saving</h3>
+              <h2 className="content-subtitle">Saving</h2>
               <p>Name your template/style using the Name Your Style input field.</p>
               <p>You may choose to use the default name of Hue with a timestamp.</p>
               <p>Click on Save Template to create a unique link to your template for later editing.</p>
               <p>Use Update Template to update your current saved template.</p>
               <p>Click on Export HTML and CSS to download a .zip file of the HTML and CSS onto your computer.</p>
-              <br />
             </div>
             <div className="flex-100">
-              <h3>Loading Saved Styles/Templates</h3>
+              <h2 className="content-subtitle">Loading Saved Styles/Templates</h2>
               <p>Click on Saved Styles in the Style Editor Sidebar.</p>
               <p>Using the Choose Your Template dropdown menu, select a template to load.</p>
             </div>
