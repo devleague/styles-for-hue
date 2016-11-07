2151633c3671e3d2ab21228b2e263bab24f00534
Lisa <misslisazhou@gmail.com>
2016-11-07 01:13:33 -1000

    move content around on Resource page

--- src/containers/Resource.js
+++ src/containers/Resource.js
@@ -15,12 +15,12 @@ class Resource extends Component {
         <div className="row">
           <div className="flex-wrap-container">
             <div className="flex-100">
-              <h2>Fonts</h2>
-              <p>For the everyday developer that needs help picking out fonts. Google Fonts have popular pairings with each fonts provided on the website. </p>
-              <p><a href="https://fonts.google.com/" target="_blank">Google Fonts</a></p>
+              <h2 className="resource-subtitle">Fonts</h2>
+              <p>For the everyday developer that needs help picking out fonts. </p>
+              <p><a href="https://fonts.google.com/" target="_blank">Google Fonts</a> - Google Fonts have popular pairings with each fonts provided on the website.</p>
             </div>
             <div className="flex-100">
-              <h2>Color Palettes</h2>
+              <h2 className="resource-subtitle">Color Palettes</h2>
               <p>A few sites that we recommend to the everyday developer that needs help with color palettes. There are more color palette sites that are out there. These sites are here to help the everyday developer start off from here.</p>
               <p><a href="http://colorpalettes.net/" target="_blank">Color Palettes</a></p>
               <p><a href="http://www.colourlovers.com/" target="_blank">Colourlovers</a></p>
@@ -30,7 +30,7 @@ class Resource extends Component {
               <p><a href="http://www.pictaculous.com/" target="_blank">Pictaculous</a> - If you want to create a site that compliments the colors of a photo.</p>
             </div>
             <div className="flex-100">
-              <h2>Responsive Grid Layout</h2>
+              <h2 className="resource-subtitle">Responsive Grid Layout</h2>
               <p>Creating a responsive layout that is visually appealing to the users. </p>
               <p><a href="http://www.responsivegridsystem.com/" target="_blank">Responsive Grid System</a></p>
             </div>
